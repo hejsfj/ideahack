442a6078ea7aa96bc1a773d10e551cecef8b078c
richardnoa <richard.noack@icloud.com>
2019-10-03 02:00:53 +0200

    axios-fire

--- smart-incidence/src/components/user/QRCodeScanned/QRCodeScanned.js
+++ smart-incidence/src/components/user/QRCodeScanned/QRCodeScanned.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from '../../../axios-data';
 
-const QRCodeScanned = (props) => (
-    <div>
-        <h1>QRCodeScanned</h1>
-        <p>We love your effort to make ...</p>
-    </div>
-);
+class QRCodeScanned extends Component {
+    state = {
+        id: 35791,
+        tag: 'trashcan',
+        filled: false,
+        location: {
+            lat: 50.398292,
+            lon: 7.613024
+        }
+    }
+
+    takePhotoHandler = () => {
+        const incidences = {
+            incidence: this.state
+        }
+
+        axios.post('/incidence.json', incidences)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>QRCodeScanned</h1>
+                <p>We love your effort to make ...</p>
+                <button onClick={this.takePhotoHandler}>Take a photo</button>
+            </div>
+        )
+    }
+};
 
 export default QRCodeScanned;
