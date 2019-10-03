b208e823c7677193d7bebf7bedd96e4893a56129
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2019-10-03 09:08:15 +0200

    Add Map Styling

--- smart-incidence/src/components/web/IncidentsMap/IncidentsMap.js
+++ smart-incidence/src/components/web/IncidentsMap/IncidentsMap.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import axios from '../../../axios-data';
+import classes from './IncidentsMap.module.css';
+
+const style = {
+  width: '100%',
+  height: '30%'
+};
 
 class IncidentsMap extends Component {
   state = {
     incidence: null
   };
+
   componentDidMount() {
     axios
       .get('https://smart-incidence.firebaseio.com/incidence.json')
       .then(response => {
-        const data = Object.values(response.data)
+        const data = Object.values(response.data);
         this.setState({ incidence: data });
       });
   }
@@ -19,30 +26,30 @@ class IncidentsMap extends Component {
     let posts = <p>No posts yet</p>;
     let mark = null;
 
-  if (this.state.incidence){
-    mark = Object.values(this.state.incidence).map((obj, id) => {
-      return (
-        <Marker 
-        key={this.state.incidence[id].incidence.id} 
-        position={this.state.incidence[id].incidence.location} />
-      )
-    });
-  };
+    if (this.state.incidence) {
+      mark = Object.values(this.state.incidence).map((obj, id) => {
+        return (
+          <Marker
+            key={this.state.incidence[id].incidence.id}
+            position={this.state.incidence[id].incidence.location}
+          />
+        );
+      });
+    }
 
     return (
       <div>
         {/* {this.state.incidence.id} */}
-        <h1>IncidentsMap</h1>
-        <p>MAP</p>
-        <p>Incidents List</p>
-
-        <Map
-          google={this.props.google}
-          zoom={14}
-        initialCenter={{lat: 50.398292, lng: 7.613024}}
-        >
-          {mark}
-        </Map>
+        <div className={classes.map}>
+          <Map
+            style={style}
+            google={this.props.google}
+            zoom={14}
+            initialCenter={{ lat: 50.398292, lng: 7.613024 }}
+          >
+            {mark}
+          </Map>
+        </div>
       </div>
     );
   }
