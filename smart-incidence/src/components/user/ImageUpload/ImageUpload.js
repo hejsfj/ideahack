9ef1dc6819ce7258b396e3b04334698c7e74e190
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2019-10-03 10:45:36 +0200

    Crazy Stuff

--- smart-incidence/src/components/user/ImageUpload/ImageUpload.js
+++ smart-incidence/src/components/user/ImageUpload/ImageUpload.js
@@ -6,29 +6,31 @@ import QrReader from 'react-qr-reader';
 class ImageUpload extends Component {
   render() {
     return (
-      <div className={classes.introtext}>
-        <h1 className={classes.h1}>Scan a new incident</h1>
+      <div className={classes.big}>
+        <div className={classes.introtext}>
+          <h1 className={classes.h1}>Scan a new incident</h1>
 
-        <h2 className={classes.h2}>
-          Spotted something strange? Take an image and send it to us! We will
-          take care of this!
-        </h2>
+          <h2 className={classes.h2}>
+            Spotted something strange? Take an image and send it to us! We will
+            take care of this!
+          </h2>
 
-        <div className={classes.container}>
-          <a href="./QRRead">
-            <div className={classes.circleQRCode}>
-              <div className={classes.icon}>
-                <FontAwesomeIcon icon="qrcode" size="2x" />
+          <div className={classes.container}>
+            <a href="./QRRead">
+              <div className={classes.circleQRCode}>
+                <div className={classes.icon}>
+                  <FontAwesomeIcon icon="qrcode" size="2x" />
+                </div>
               </div>
-            </div>
-          </a>
-          <a href="./AddImage">
-            <div className={classes.circleCam}>
-              <div className={classes.icon}>
-                <FontAwesomeIcon icon="camera" size="4x" />
+            </a>
+            <a href="./AddImage">
+              <div className={classes.circleCam}>
+                <div className={classes.icon}>
+                  <FontAwesomeIcon icon="camera" size="4x" />
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </div>
         </div>
       </div>
     );
