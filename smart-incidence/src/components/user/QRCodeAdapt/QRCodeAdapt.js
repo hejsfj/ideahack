c9f4315a76552e8f0d16004b016015617356b1a5
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2019-10-03 06:03:00 +0200

    Fix

--- smart-incidence/src/components/user/QRCodeAdapt/QRCodeAdapt.js
+++ smart-incidence/src/components/user/QRCodeAdapt/QRCodeAdapt.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import classes from './QRCodeAdapt.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const QRCodeAdapt = (props) => (
-    <div>
-        <h1>QRCodeAdapt</h1>
-        <p>Your incident has been submitted. ...</p>
+const QRCodeAdapt = props => (
+  <div className={classes.warpper}>
+    <div className="magic-circle"></div>
+    <div className={classes.introtext}></div>
+    <h1>CHECK</h1>
+    <h2 className={classes.h2}>
+      Your incident has been submitted. We think it is a defect bulb, could this
+      be correct?
+    </h2>
+
+    <div className={classes.yes}>
+      <div className={classes.icon}>YES</div>
     </div>
+    <div className={classes.no}>
+      <div className={classes.icon}>No</div>
+    </div>
+  </div>
 );
 
 export default QRCodeAdapt;
